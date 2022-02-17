4964e73c26681805ca7231f263085d9691920357
Edward Upton <ed.upton@outlook.com>
2022-02-17 18:11:33 +0000

    Started badges logic

---
+++ server/src/entities/Badge.ts
@@ -0,0 +1,33 @@
+import { ObjectType, Field } from "type-graphql";
+import {
+  Entity,
+  BaseEntity,
+  PrimaryGeneratedColumn,
+  Column,
+  CreateDateColumn,
+  UpdateDateColumn,
+} from "typeorm";
+
+@ObjectType()
+@Entity()
+export class Badge extends BaseEntity {
+  @Field()
+  @PrimaryGeneratedColumn()
+  id: number;
+
+  @Field(() => String)
+  @CreateDateColumn()
+  createdAt: Date;
+
+  @Field(() => String)
+  @UpdateDateColumn()
+  updatedAt: Date;
+
+  @Field()
+  @Column()
+  title: string;
+
+  @Field()
+  @Column({ unique: true })
+  shortName: string;
+}
