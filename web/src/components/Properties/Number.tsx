c7d30b3e2c6314a52b4cd2984000fcc245d2dc53
Edward Upton <ed.upton@outlook.com>
2022-08-10 23:03:00 +0100

    Update Number property in-line with Text

--- web/src/components/Properties/Number.tsx
+++ web/src/components/Properties/Number.tsx
@@ -1,22 +1,67 @@
-import { Input, Text } from "@chakra-ui/react";
+import { Flex, Input, Text } from "@chakra-ui/react";
+import { useEffect, useState } from "react";
 
 interface NumberPropertyProps {
   value: number;
   onChange: (v: number) => void;
   isEdit: boolean;
+  isTitle?: boolean;
 }
 
 const NumberProperty: React.FC<NumberPropertyProps> = (props) => {
+  const [value, setValue] = useState<string>(props.value + "");
+
+  useEffect(() => {
+    setValue(props.value + "");
+  }, [props.value]);
+
   if (!props.isEdit) {
-    return <Text>{props.value}</Text>;
+    return (
+      <Flex
+        width="full"
+        minWidth={0}
+        height={props.isTitle ? 14 : 10}
+        paddingInlineStart={4}
+        paddingInlineEnd={4}
+        alignItems="center"
+        borderWidth={1}
+        borderRadius="md"
+        borderColor="white"
+      >
+        <Text
+          fontWeight={props.isTitle ? "bold" : "normal"}
+          fontSize={props.isTitle ? "2xl" : "md"}
+          width="full"
+          minWidth={0}
+          noOfLines={1}
+        >
+          {parseFloat(value)}
+        </Text>
+      </Flex>
+    );
   } else {
     return (
       <Input
-        value={props.value}
+        value={value}
         type={"text"}
         onChange={async (e) => {
-          props.onChange(parseInt(e.target.value));
+          // Check if last character is one of [. , 0]
+          if (
+            [".", ",", "0"].includes(
+              e.target.value.charAt(e.target.value.length - 1)
+            )
+          ) {
+            setValue(e.target.value);
+          } else {
+            // Remove commas
+            e.target.value = e.target.value.replace(/,/g, "");
+            setValue(parseFloat(e.target.value) + "");
+            props.onChange(parseFloat(e.target.value));
+          }
         }}
+        height={props.isTitle ? 14 : 10}
+        fontSize={props.isTitle ? "2xl" : "md"}
+        fontWeight={props.isTitle ? "bold" : "normal"}
       />
     );
   }
