ea38fa2e2888d27b8c3105beb195cd49f972a375
Edward Upton <ed.upton@outlook.com>
2022-08-20 15:36:32 +0100

    Formatting

--- web/src/utils/betterUpdateQuery.ts
+++ web/src/utils/betterUpdateQuery.ts
@@ -2,9 +2,10 @@ import { Cache, QueryInput } from "@urql/exchange-graphcache";
 
 // Making cache exchange function types better
 export function betterUpdateQuery<Result, Query>(
-    cache: Cache,
-    qi: QueryInput,
-    result: any,
-    fn: (r: Result, q: Query) => Query) {
-    return cache.updateQuery(qi, (data) => fn(result, data as any) as any);
+  cache: Cache,
+  qi: QueryInput,
+  result: any,
+  fn: (r: Result, q: Query) => Query
+) {
+  return cache.updateQuery(qi, (data) => fn(result, data as any) as any);
 }
