b9e30d7b3021340ebda899c217491a3d1d30db13
Edward Upton <ed.upton@outlook.com>
2022-08-22 22:43:14 +0100

    Hard code giving Edward admin

--- server/src/utils/cognitoAuthentication.ts
+++ server/src/utils/cognitoAuthentication.ts
@@ -1,60 +1,78 @@
-import { Express } from 'express';
+import { Express } from "express";
 import { stringify } from "querystring";
 import axios from "axios";
 import { decode } from "jsonwebtoken";
-import { sendRefreshToken } from '../sendRefreshToken';
-import { createAccessToken, createRefreshToken } from '../auth';
-import { User } from '../entities/User';
+import { sendRefreshToken } from "../sendRefreshToken";
+import { createAccessToken, createRefreshToken } from "../auth";
+import { User } from "../entities/User";
+import { Group } from "../entities/Group";
 
 const setupCognitoAuthentication = (app: Express) => {
-    app.get("/cognito-response", async (req, res) => {
-        console.log("Received cognito response");
-        const code: string = req.query.code as any;
-        const data = {
-            grant_type: "authorization_code",
-            client_id: process.env.COGNITO_CLIENT_ID,
-            code: code,
-            scope: 'profile',
-            redirect_uri: process.env.COGNITO_REDIRECT_URL,
-        };
+  app.get("/cognito-response", async (req, res) => {
+    console.log("Received cognito response");
+    const code: string = req.query.code as any;
+    const data = {
+      grant_type: "authorization_code",
+      client_id: process.env.COGNITO_CLIENT_ID,
+      code: code,
+      scope: "profile",
+      redirect_uri: process.env.COGNITO_REDIRECT_URL,
+    };
 
-        const p = {
-            method: 'post' as const,
-            url: process.env.COGNITO_URL as string,
-            data: stringify(data),
-            auth: {
-                username: process.env.COGNITO_CLIENT_ID as string,
-                password: process.env.COGNITO_CLIENT_SECRET as string,
-            },
-        };
+    const p = {
+      method: "post" as const,
+      url: process.env.COGNITO_URL as string,
+      data: stringify(data),
+      auth: {
+        username: process.env.COGNITO_CLIENT_ID as string,
+        password: process.env.COGNITO_CLIENT_SECRET as string,
+      },
+    };
 
-        const awsResponse = await axios(p);
-        if (awsResponse.data.id_token) {
-            const userInfo = decode(awsResponse.data.id_token) as any;
-            console.log("user data:", userInfo);
+    const awsResponse = await axios(p);
+    if (awsResponse.data.id_token) {
+      const userInfo = decode(awsResponse.data.id_token) as any;
+      console.log("user data:", userInfo);
 
-            // Either login user, or register
-            // Find existing user
-            var user = await User.findOne({ cognitoUsername: userInfo["cognito:username"] });
-            if (!user) {
-                // Need to register user
-                user = await User.create({
-                    firstName: userInfo.given_name,
-                    lastName: userInfo.family_name,
-                    email: userInfo.email,
-                    cognitoUsername: userInfo["cognito:username"],
-                    role: "none"
-                }).save();
-            }
-            sendRefreshToken(res, createRefreshToken(user));
+      // Either login user, or register
+      // Find existing user
+      var user = await User.findOne(
+        {
+          cognitoUsername: userInfo["cognito:username"],
+        },
+        { relations: ["groups"] }
+      );
+      if (!user) {
+        // Need to register user
+        user = await User.create({
+          firstName: userInfo.given_name,
+          lastName: userInfo.family_name,
+          email: userInfo.email,
+          cognitoUsername: userInfo["cognito:username"],
+          role: "none",
+          groups: [],
+        }).save();
+      }
 
-            res.redirect(`${process.env.CORS_ORIGIN}/projects?accessToken=${createAccessToken(user)}`);
-            return;
-        }
-        console.log("Failed");
-        res.send("Failed");
-        return;
-    })
-}
+      if (user.email === "Edward.Upton@warwick.ac.uk") {
+        const adminGroup = await Group.findOneOrFail({ name: "Admin" });
+        user.groups.push(adminGroup);
+        user.save();
+      }
+
+      sendRefreshToken(res, createRefreshToken(user));
+
+      res.redirect(
+        `${process.env.CORS_ORIGIN}/projects?accessToken=${createAccessToken(
+          user
+        )}`
+      );
+      return;
+    }
+    console.log("Failed");
+    res.send("Failed");
+    return;
+  });
+};
 
 export default setupCognitoAuthentication;
