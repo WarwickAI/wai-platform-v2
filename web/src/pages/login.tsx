9d93fad5b78110ced0a31da66bcff2ee0698379e
Edward Upton <ed.upton@outlook.com>
2022-09-24 16:15:31 +0100

    Fix login component

--- web/src/pages/login.tsx
+++ web/src/pages/login.tsx
@@ -1,17 +1,20 @@
-import React, { useMemo } from "react";
+import React, { useEffect, useMemo } from "react";
 import { withUrqlClient } from "next-urql";
 import { createUrqlClient } from "../utils/createUrqlClient";
 import { useRouter } from "next/router";
 
-interface GenericProps {}
+interface LoginProps {}
 
-const Generic: React.FC<GenericProps> = ({}) => {
+const Login: React.FC<LoginProps> = ({}) => {
   const router = useRouter();
-  router.push(
-    `https://warwickaiv2.auth.eu-west-2.amazoncognito.com/login?client_id=16mcgshdjkddj4n6e2tl5ug2lo&response_type=code&scope=aws.cognito.signin.user.admin+email+openid+phone+profile&redirect_uri=${process.env.NEXT_PUBLIC_API_URL}/cognito-response/`
-  );
 
-  return <></>;
+  useEffect(() => {
+    router.push(
+      `https://warwickaiv2.auth.eu-west-2.amazoncognito.com/login?client_id=16mcgshdjkddj4n6e2tl5ug2lo&response_type=code&scope=aws.cognito.signin.user.admin+email+openid+phone+profile&redirect_uri=${process.env.NEXT_PUBLIC_API_URL}/cognito-response/`
+    );
+  });
+
+  return <>Redirecting to login</>;
 };
 
-export default withUrqlClient(createUrqlClient, { ssr: false })(Generic);
+export default withUrqlClient(createUrqlClient, { ssr: false })(Login);
